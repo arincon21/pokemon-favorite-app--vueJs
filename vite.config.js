85a8c632758a68a566d5328d5d434316b13a2a25
Alvaro Rincon <arincon@outlook.com>
2025-08-07 21:36:35 -0500

    Add: Configurar gh-pages y ajustar rutas en el enrutador

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/pokemon-favorite-app--vueJs/',
   plugins: [vue()],
   test: {
     globals: true,
